91e4c00190083950e3ce4746a931adf3d47fe768
Gurpreet Randhawa <gurpreetpro@github.com>
2026-03-11 10:55:40 +0530

    fix: aiRoutes.js was importing deleted aiControllerNew - changed to aiController

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const aiController = require('../controllers/aiControllerNew');
+const aiController = require('../controllers/aiController');
 const aiService = require('../services/aiService');
 const { authenticateToken } = require('../middleware/authMiddleware');
 
